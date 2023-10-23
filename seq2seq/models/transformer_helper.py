98fcfc60892607d8f15aa8c5de05a5cae20cee02
daniel <mrlixuran@126.com>
2023-10-23 20:52:53 +0800

    all

---
+++ seq2seq/models/transformer_helper.py
@@ -0,0 +1,333 @@
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from seq2seq import utils
+
+
+class TransformerEncoderLayer(nn.Module):
+    def __init__(self, args):
+        super().__init__()
+        self.embed_dim = args.encoder_embed_dim
+        self.self_attn = MultiHeadAttention(
+            self.embed_dim,
+            args.encoder_attention_heads,
+            dropout=args.attention_dropout,
+            self_attention=True
+        )
+        self.self_attn_layer_norm = LayerNorm(self.embed_dim)
+        self.dropout = args.attention_dropout
+        self.activation_dropout = args.activation_dropout
+
+        self.fc1 = generate_linear(self.embed_dim, args.encoder_ffn_embed_dim)
+        self.fc2 = generate_linear(args.encoder_ffn_embed_dim, self.embed_dim)
+        self.final_layer_norm = LayerNorm(self.embed_dim)
+
+    def forward(self, state, encoder_padding_mask):
+        """Forward pass of a single Transformer Encoder Layer"""
+        residual = state.clone()
+
+        '''
+        ___QUESTION-7-DESCRIBE-D-START___
+        What is the purpose of encoder_padding_mask? What will the output shape of `state' Tensor 
+        be after multi-head attention? HINT: formulate your  answer in terms of 
+        constituent variables like batch_size, embed_dim etc...
+        '''
+        #the encoder_padding_mask is used to mask out all the padding done during data sampling.
+        #the state in the input to the forward funciton has shape [src_time_steps, batch_size, num_features]
+        # it can be inferred from the shape requirement of the self_attn_layer_norm layer that the output shape of the "state"
+        #tensor should be [src_time_steps, batch_size, embed_dim]
+        state, _ = self.self_attn(query=state, key=state, value=state, key_padding_mask=encoder_padding_mask)
+        '''
+        ___QUESTION-7-DESCRIBE-D-END___
+        '''
+        #print(state.shape)
+        #print(residual.shape)
+        state = F.dropout(state, p=self.dropout, training=self.training)
+        state += residual
+        state = self.self_attn_layer_norm(state)
+
+        residual = state.clone()
+        state = F.relu(self.fc1(state))
+        state = F.dropout(state, p=self.activation_dropout, training=self.training)
+        state = self.fc2(state)
+        state = F.dropout(state, p=self.dropout, training=self.training)
+        state += residual
+        state = self.final_layer_norm(state)
+        #print("encoder final ",state.shape)
+        return state
+
+
+class TransformerDecoderLayer(nn.Module):
+    def __init__(self, args):
+        super().__init__()
+        self.embed_dim = args.decoder_embed_dim
+        self.dropout = args.attention_dropout
+        self.activation_dropout = args.activation_dropout
+        self.self_attn_layer_norm = LayerNorm(self.embed_dim)
+
+        self.self_attn = MultiHeadAttention(
+            embed_dim=self.embed_dim,
+            num_attn_heads=args.decoder_attention_heads,
+            dropout=args.attention_dropout,
+            self_attention=True
+        )
+
+        self.encoder_attn = MultiHeadAttention(
+            embed_dim=self.embed_dim,
+            num_attn_heads=args.decoder_attention_heads,
+            kdim=args.encoder_embed_dim,
+            vdim=args.encoder_embed_dim,
+            dropout=args.attention_dropout,
+            encoder_decoder_attention=True,
+        )
+
+        self.encoder_attn_layer_norm = LayerNorm(self.embed_dim)
+        self.fc1 = generate_linear(self.embed_dim, args.decoder_ffn_embed_dim)
+        self.fc2 = generate_linear(args.decoder_ffn_embed_dim, self.embed_dim)
+        self.final_layer_norm = LayerNorm(self.embed_dim)
+        self.need_attn = True
+
+    def forward(self,
+                state,
+                encoder_out=None,
+                encoder_padding_mask=None,
+                incremental_state=None,
+                prev_self_attn_state=None,
+                self_attn_mask=None,
+                self_attn_padding_mask=None,
+                need_attn=False,
+                need_head_weights=False):
+        """Forward pass of a single Transformer Decoder Layer"""
+
+        # need_attn must be True if need_head_weights
+        need_attn = True if need_head_weights else need_attn
+
+        residual = state.clone()
+        state, _ = self.self_attn(query=state,
+                                  key=state,
+                                  value=state,
+                                  key_padding_mask=self_attn_padding_mask,
+                                  need_weights=False,
+                                  attn_mask=self_attn_mask)
+        state = F.dropout(state, p=self.dropout, training=self.training)
+        state += residual
+        state = self.self_attn_layer_norm(state)
+
+        residual = state.clone()
+        '''
+        ___QUESTION-7-DESCRIBE-E-START___
+        How does encoder attention differ from self attention? What is the difference between key_padding_mask 
+        and attn_mask? If you understand this difference, then why don't we need to give attn_mask here?
+        '''
+        # instead of using embed_dim as in self attention, encode attention layer use a pre-specified dimension for its value matrix V and Key matrix K.
+        # the attn_mask is used to mask out the part of KEY_QUERY matrix that shouldn't be seen during the decoding stage, so that the model can not see forward in the input and
+        # learn to copy the next word.
+        # the key_padding_mask is used to mask out the padding in the key vector, which is previously added to normalise the length of the sentences.
+        # previously the attn_mask is needed so that when decoding the decoder can't look forward in the target sentence.
+        # now in this layer the model is trying to learn the correspondence between the hidden vector of the target sequence with the output of the encoder.
+        # as we should not restrict the mapping from the source sentence to the target sentence to those words that s before the current target word, we dont want a attn_mask here.
+
+        state, attn = self.encoder_attn(query=state,
+                                        key=encoder_out,
+                                        value=encoder_out,
+                                        key_padding_mask=encoder_padding_mask,
+                                        need_weights=need_attn or (not self.training and self.need_attn))
+        '''
+        ___QUESTION-7-DESCRIBE-E-END___
+        '''
+
+        state = F.dropout(state, p=self.dropout, training=self.training)
+        state += residual
+        state = self.encoder_attn_layer_norm(state)
+
+        residual = state.clone()
+        state = F.relu(self.fc1(state))
+        state = F.dropout(state, p=self.activation_dropout, training=self.training)
+        state = self.fc2(state)
+        state = F.dropout(state, p=self.dropout, training=self.training)
+        state += residual
+        state = self.final_layer_norm(state)
+
+        return state, attn
+
+
+class MultiHeadAttention(nn.Module):
+    """Multi-Head Attention"""
+    def __init__(self,
+                 embed_dim,
+                 num_attn_heads,
+                 kdim=None,
+                 vdim=None,
+                 dropout=0.,
+                 self_attention=False,
+                 encoder_decoder_attention=False):
+        '''
+        ___QUESTION-8-MULTIHEAD-ATTENTION-NOTE
+        You shouldn't need to change the __init__ of this class for your attention implementation
+        '''
+        super().__init__()
+        self.embed_dim = embed_dim
+        self.k_embed_size = kdim if kdim else embed_dim
+        self.v_embed_size = vdim if vdim else embed_dim
+
+        self.num_heads = num_attn_heads
+        self.attention_dropout = dropout
+        self.head_embed_size = embed_dim // num_attn_heads  # this is d_k in the paper
+        self.head_scaling = math.sqrt(self.head_embed_size)
+
+        self.self_attention = self_attention
+        self.enc_dec_attention = encoder_decoder_attention
+
+        kv_same_dim = self.k_embed_size == embed_dim and self.v_embed_size == embed_dim
+        assert self.head_embed_size * self.num_heads == self.embed_dim, "Embed dim must be divisible by num_heads!"
+        assert not self.self_attention or kv_same_dim, "Self-attn requires query, key and value of equal size!"
+        assert self.enc_dec_attention ^ self.self_attention, "One of self- or encoder- attention must be specified!"
+
+        self.k_proj = nn.Linear(self.k_embed_size, embed_dim, bias=True)
+        self.v_proj = nn.Linear(self.v_embed_size, embed_dim, bias=True)
+        self.q_proj = nn.Linear(self.k_embed_size, embed_dim, bias=True) # I believe there s a bug here the input size should be embed_dim
+        self.out_proj = nn.Linear(self.embed_dim, self.embed_dim, bias=True)
+
+        # Xavier initialisation
+        nn.init.xavier_uniform_(self.k_proj.weight, gain=1 / math.sqrt(2))
+        nn.init.xavier_uniform_(self.v_proj.weight, gain=1 / math.sqrt(2))
+        nn.init.xavier_uniform_(self.q_proj.weight, gain=1 / math.sqrt(2))
+        nn.init.xavier_uniform_(self.q_proj.weight, gain=1 / math.sqrt(2))
+        nn.init.xavier_uniform_(self.out_proj.weight)
+        nn.init.constant_(self.out_proj.bias, 0.)
+
+    def forward(self,
+                query,
+                key,
+                value,
+                key_padding_mask=None,
+                attn_mask=None,
+                need_weights=True):
+
+        # Get size features
+        tgt_time_steps, batch_size, embed_dim = query.size()
+        src_time_steps = value.size(0)
+        # Q: [t,b,dk] (dk=embed)
+        # K: [s,b,dk]
+        # V: [s,b,dv]
+
+        assert self.embed_dim == embed_dim
+
+        '''
+        ___QUESTION-8-MULTIHEAD-ATTENTION-START
+        Implement Multi-Head attention  according to Section 3.2.2 of https://arxiv.org/pdf/1706.03762.pdf.
+        Note that you will have to handle edge cases for best model performance. Consider what behaviour should
+        be expected if attn_mask or key_padding_mask are given?
+        '''
+
+        # attn is the output of MultiHead(Q,K,V) in Vaswani et al. 2017
+        # attn must be size [tgt_time_steps, batch_size, embed_dim]
+        # attn_weights is the combined output of h parallel heads of Attention(Q,K,V) in Vaswani et al. 2017
+        # attn_weights must be size [num_heads, batch_size, tgt_time_steps, key.size(0)]
+
+        t,b,h,k,s=tgt_time_steps,batch_size,self.num_heads,self.head_embed_size,src_time_steps
+        #print("tbhk ",t,b,h,k)
+        #print("vk embeding",self.v_embed_size,self.k_embed_size)
+        if (type(key_padding_mask) != type(None)):
+            key_padding_mask=key_padding_mask.transpose(0,1)
+            key_padding_mask=key_padding_mask.unsqueeze(2)
+            key_padding_mask=key_padding_mask.expand(s,b,self.k_embed_size)
+            key=key*key_padding_mask
+
+        Wq = self.q_proj(query).view(t,b,h,k)    #[tgt_time_steps, batch_size,embed_dim] -> [t,b,h,k]
+        Wv = self.v_proj(value).view(s,b,h,k)    #[s,b,h,k]
+        Wk = self.k_proj( key ).view(s,b,h,k)    #[t,b,h,k]
+        Wq = Wq.transpose(0, 1).transpose(1,2).view(b*h,t,k)     #[b*h,t,k]
+        Wv = Wv.transpose(0, 1).transpose(1, 2).view(b*h,s,k)
+        Wk = Wk.transpose(0, 1).transpose(1, 2).view(b*h,s,k)
+
+        dot = torch.bmm( Wq , Wk.transpose(1,2))/self.head_scaling #[b*h,t,s]
+
+
+
+        if(type(attn_mask)!= type(None)):
+            #print(attn_mask)
+            #print("attn_mask: ", attn_mask.shape)
+            attn_mask=attn_mask.unsqueeze(0)
+            attn_mask=attn_mask.expand(b*h,t,t)
+            dot = dot*attn_mask
+        dot = F.softmax( dot , dim=2)
+        out = torch.bmm( dot , Wv).view(b,h,t,k)     #[b,h,t,k]
+        out = out.transpose(1, 2).transpose(0, 1).contiguous().view(t, b, h * k)
+        attn = self.out_proj(out)
+        attn_weights = out.view(b,t,h,k).transpose(2,0).transpose(1,2)
+
+        '''
+        ___QUESTION-8-MULTIHEAD-ATTENTION-END
+        '''
+
+        return attn, attn_weights
+
+
+class PositionalEmbedding(nn.Module):
+    def __init__(self, embed_dim, padding_idx, init_size=1024):
+        super().__init__()
+        self.embed_dim = embed_dim
+        self.padding_idx = padding_idx
+        self.weights = PositionalEmbedding.get_embedding(init_size, embed_dim, padding_idx)
+        self.register_buffer("_float_tensor", torch.FloatTensor(1))
+
+    @staticmethod
+    def get_embedding(num_embeddings, embed_dim, padding_idx=None):
+        half_dim = embed_dim // 2
+        emb = math.log(10000) / (half_dim - 1)
+        emb = torch.exp(torch.arange(half_dim, dtype=torch.float) * -emb)
+        emb = torch.arange(num_embeddings, dtype=torch.float).unsqueeze(1) * emb.unsqueeze(0)
+        emb = torch.cat([torch.sin(emb), torch.cos(emb)], dim=1).view(num_embeddings, -1)
+        if embed_dim % 2 == 1:
+            # Zero pad in specific mismatch case
+            emb = torch.cat([emb, torch.zeros(num_embeddings, 1)], dim=1)
+        if padding_idx is not None:
+            emb[padding_idx, :] = 0.
+        return emb
+
+    def forward(self, inputs, incremental_state=None, timestep=None):
+        batch_size, seq_len = inputs.size()
+        max_pos = self.padding_idx + 1 + seq_len
+
+        if self.weights is None or max_pos > self.weights.size(0):
+            # Expand embeddings if required
+            self.weights = PositionalEmbedding.get_embedding(max_pos, self.embed_dim, self.padding_idx)
+        self.weights = self.weights.to(self._float_tensor)
+
+        if incremental_state is not None:
+            #   Positional embed is identical for all tokens during single step decoding
+            pos = timestep.view(-1)[0] + 1 if timestep is not None else seq_len
+            return self.weights.index_select(index=self.padding_idx + pos, dim=0).unsqueeze(1).repeat(batch_size, 1, 1)
+
+        # Replace non-padding symbols with position numbers from padding_idx+1 onwards.
+        mask = inputs.ne(self.padding_idx).int()
+        positions = (torch.cumsum(mask, dim=1).type_as(inputs) * mask).long() + self.padding_idx
+
+        # Lookup positional embeddings for each position and return in shape of input tensor w/o gradient
+        return self.weights.index_select(0, positions.view(-1)).view(batch_size, seq_len, -1).detach()
+
+
+def LayerNorm(normal_shape, eps=1e-5):
+    return torch.nn.LayerNorm(normalized_shape=normal_shape, eps=eps, elementwise_affine=True)
+
+
+def fill_with_neg_inf(t):
+    return t.float().fill_(float('-inf')).type_as(t)
+
+
+def generate_embedding(num_embeddings, embedding_dim, padding_idx):
+    m = nn.Embedding(num_embeddings, embedding_dim, padding_idx=padding_idx)
+    nn.init.normal_(m.weight, mean=0, std=embedding_dim ** -0.5)
+    nn.init.constant_(m.weight[padding_idx], 0)
+    return m
+
+
+def generate_linear(in_features, out_features, bias=True):
+    m = nn.Linear(in_features, out_features, bias)
+    nn.init.xavier_uniform_(m.weight)
+    if bias:
+        nn.init.constant_(m.bias, 0.)
+    return m
